5c8cee936e8799ab32f13e2ce03b83cd599e2f6f
shahbaz8 <muhammadshahbaz9934@gmail.com>
2018-01-07 16:32:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/itcomputerszone/uolfallcustomadapter/UserAdapter.java
@@ -0,0 +1,63 @@
+package com.example.itcomputerszone.uolfallcustomadapter;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.itcomputerszone.uolfallcustomadapter.R;
+import com.example.itcomputerszone.uolfallcustomadapter.models.User;
+
+import java.util.List;
+
+/**
+ * Created by IT Computer's Zone on 1/7/2018.
+ */
+
+public class UserAdapter extends ArrayAdapter<user> {
+    private static final String TAG = "MTAG";
+
+    class ViewHolder{
+        TextView tvUsername;
+        TextView tvRollNumber;
+
+    }
+    public UserAdapter(@NonNull Context context, @NonNull List<User> objects) {
+        super(context,0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Log.d(TAG,"convert view "+convertView);
+        View view=convertView;
+        ViewHolder vh;
+        User u=getItem(position);
+        if(view==null)
+        {
+            view= LayoutInflater.from(getContext()).inflate(R.layout.user_list_item,parent,false);
+            TextView tvUsername= (TextView) view.findViewById(R.id.tvUsername);
+            TextView tvRollNumber= (TextView) view.findViewById(R.id.tvRollNumber);
+
+            vh=new ViewHolder();
+            vh.tvUsername= (TextView) view.findViewById(R.id.tvUsername);
+            vh.tvRollNumber= (TextView) view.findViewById(R.id.tvRollNumber);
+            view.setTag(vh);
+        }
+        vh= (ViewHolder) view.getTag();
+        vh.tvUsername.setText(u.getUsername());
+        vh.tvRollNumber.setText(u.getRollnumber());
+
+
+
+        return  view;
+    }
+}
+
---
+++ app/src/test/java/com/example/itcomputerszone/uolfallcustomadapter/MainActivityTest.java
@@ -0,0 +1,8 @@
+package com.example.itcomputerszone.uolfallcustomadapter;
+
+/**
+ * Created by IT Computer's Zone on 1/7/2018.
+ */
+
+public class MainActivityTest {
+}
